185d2d653bddebfc6c68a12a1b9ff0f751638b66
Lihkin <shniks@gmail.com>
2018-01-02 22:56:53 -0700

    Started Denver Library Project.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,21 @@
+require './lib/book'
+
 class Author
+
+  attr_reader :books
+
+  def initialize(author)
+
+    @books = []
+
+  end
+
+  def write_book(title, year)
+    @books << Book.new
+    
+
+  end
+
+
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,27 @@
 require './test/test_helper'
+require 'minitest/pride'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
+
+  def test_if_it_exists
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_instance_of Author, charlotte_bronte
+  end
+
+  def test_if_books_array_is_empty
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal [], charlotte_bronte.books
+  end
+
+  # def test_one_book
+  #   charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  #   charlotte_bronte.write_book("Jane Eyre", "October 16, 1847")
+  #
+  #   assert_equal , charlotte_bronte.books
+  # end
 
 
 end
